3ad8974faa54135fd330723a6627913eee0fe4a6
HeatherJane <heatherjane.moore@gmail.com>
2020-05-20 13:15:18 -0400

    updated

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "5de06d208a0929f59052380157e35b8d"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyA_B0wmNyAyKTFj_Ub-ToIkrVkunn9HgU4"
+g_key = ""
